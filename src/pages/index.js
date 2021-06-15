d7710112eb270221e7b28b2da6676d1e9c53763d
Samir Aliyev <aliyev7136@gmail.com>
2021-06-14 18:52:08 -0700

    Banner updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
 
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from '../sections/banner';
-import KeyFeature from '../sections/key-feature';
-import ServiceSection from '../sections/service-section';
-import Feature from '../sections/feature';
-import CoreFeature from '../sections/core-feature';
-import WorkFlow from '../sections/workflow';
-import Package from '../sections/package';
-import TeamSection from '../sections/team-section';
-import TestimonialCard from '../sections/testimonial';
-import Blogs from 'sections/blogs';
-import FAQ from 'sections/faq';
-import Subscribe from 'sections/subscribe';
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "../sections/banner";
+import KeyFeature from "../sections/key-feature";
+import ServiceSection from "../sections/service-section";
+import Feature from "../sections/feature";
+import CoreFeature from "../sections/core-feature";
+import WorkFlow from "../sections/workflow";
+import Package from "../sections/package";
+import TeamSection from "../sections/team-section";
+import TestimonialCard from "../sections/testimonial";
+import Blogs from "sections/blogs";
+import FAQ from "sections/faq";
+import Subscribe from "sections/subscribe";
 
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
-        <Layout>
-          <SEO title="MyBlossom" />
-          <Banner />
-          <KeyFeature />
-          <ServiceSection />
-          <Feature />
-          <CoreFeature />
-          <WorkFlow />
-          <Package />
-          <TeamSection />
-          <TestimonialCard />
-          <Blogs />
-          <FAQ />
-          <Subscribe />
-        </Layout>
+      <Layout>
+        <SEO title="MyBlossom" />
+        <Banner />
+        <KeyFeature />
+        <ServiceSection />
+        <Feature />
+        <CoreFeature />
+        <WorkFlow />
+        <Package />
+        <TeamSection />
+        <TestimonialCard />
+        {/* <Blogs />  */}
+        <FAQ />
+        <Subscribe />
+      </Layout>
     </ThemeProvider>
   );
 }
