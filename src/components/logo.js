399d41bd6f77be475ea99548203049312430fc74
Samir Aliyev <63018640+samiraliyev573@users.noreply.github.com>
2021-09-19 14:17:51 -0700

    logo fixed

--- src/components/logo.js
+++ src/components/logo.js
@@ -5,12 +5,14 @@ import { Link } from 'components/link';
 export default function Logo({ src, ...rest }) {
   return (
     <Link path="/" sx={{ 
+      width: 80,
+      height: 50,
       variant: 'links.logo', 
       display: 'flex', 
       cursor: 'pointer', 
       mr: 15,}} {...rest}
       >
-        <Image src={src} alt="My Blossom landing page logo" />
+        <Image src={src} alt="Age Link landing page logo" />
       </Link>
   );
 }
